874c62e10e037c7fbb430633eebd4e6edd884a08
billyott <willrichott@gmail.com>
2020-09-15 21:57:53 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,37 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      return true
+    else
+      return false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      return true
+    else
+      return false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      return true
+    else
+      return false
+    end
   end
 
   def count_sentences
-
+    array = self.split(/[.!?]/)
+    array.each do |i|
+      if i.length == 0
+        array.delete(i)
+      end
+    end
+    array.count
   end
+
 end
